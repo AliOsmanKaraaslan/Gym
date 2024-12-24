d557e8fb98118eefb95517817e9ad1eaad749f39
Ali Osman Karaaslan <130135578+AliOsmanKaraaslan@users.noreply.github.com>
2024-12-24 20:43:54 +0300

    Login Test
    creating a new class as a login and simple test.

--- Gym/Gym/Form1.cs
+++ Gym/Gym/Form1.cs
@@ -1,10 +1,24 @@
 namespace Gym
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
+            // FormClosing olayýný elle baðlayalým
+            this.FormClosing += Form1_FormClosing;
         }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            // Eðer Form1 kapanýyorsa, diðer tüm formlarý kapatýyoruz.
+            Application.Exit();  // Uygulamayý sonlandýrýr, tüm formlar kapanýr
+        }
+
     }
 }
---
+++ Gym/Gym/login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Gym {
+    partial class login {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            textBox1.Location = new Point(176, 24);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(210, 34);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(33, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(33, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Şifre:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            textBox2.Location = new Point(176, 64);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(210, 34);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.ForeColor = Color.FromArgb(0, 192, 0);
+            button1.Location = new Point(137, 157);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 58);
+            button1.TabIndex = 4;
+            button1.Text = "Giriş";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(420, 310);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "login";
+            Load += login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
